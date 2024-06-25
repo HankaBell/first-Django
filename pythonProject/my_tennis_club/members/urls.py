0b230f11518faf3bd2bfb379d3f0a3e3507eb57e
HankaBell <beller@seznam.cz>
2024-06-25 19:53:03 +0200

    dalsi

---
+++ pythonProject/my_tennis_club/members/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.members, name='members'),
+    path('new_page/', views.new_page, name='new_page'),
+]
